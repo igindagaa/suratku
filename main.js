d94419d7b8d4dc0e18a1b12f18c62e6b1e54645b
igindagaa <indariska0@gmail.com>
2024-02-28 09:48:09 +0700

    projek bqru

---
+++ main.js
@@ -0,0 +1,32 @@
+// let nama = 'inda'
+// for(let i = 1; i<=10; i++){
+//     console.log(nama)
+// }
+
+// function inda(){
+//     let name = ['inda','riska', "dayat"]
+//     let kelas = 5
+//     for(let i = 0; i < name.length; i++){
+//         console.log(name[i],kelas++)
+//     }
+// }
+// inda()
+
+
+//asyncrhonus
+console.log(111)
+setTimeout( ()=> {
+    console.log(222)
+},3000)
+console.log(333) 
+
+function tambah(angka){
+    return angka + angka
+}
+
+console.log(tambah(12))
+
+function kali(angka){
+    return angka * 10
+}
+console.log(kali(10))
